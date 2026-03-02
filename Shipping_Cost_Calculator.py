bf37b08db3443109fc9fb4c46470f6aee5475531
Romario246 <romario.d.rock@gmail.com>
2026-03-02 15:18:38 -0400

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,6 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+ # Here is a new update by Romario246
+git config --global user.email "Romario.d.rock@gmail.com
+git config --global user.name "Romario246"
